62ce360147ac1a094ad13f58c478f477b557bde3
suresh-996 <sureshrz276@gmail.com>
2024-08-07 12:55:39 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -36,5 +36,10 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
      System.out.println("pythonlife-devops");
+    System.out.println("pythonlife-devops");
+    System.out.println("pythonlife-devops");
+    System.out.println("pythonlife-devops");
+    System.out.println("pythonlife-devops");
+    System.out.println("pythonlife-devops");
   }
 }
